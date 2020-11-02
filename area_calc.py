a111e7a03808cb0c01d34610c8154dcc40493e3a
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-11-02 15:35:44 +0100

    first commit

---
+++ area_calc.py
@@ -0,0 +1,11 @@
+#this is comment in python
+
+#name = input('Tell me your name: ')
+
+#print('welcome ' + name)
+
+radius = input('What is the Radius of the circle?')
+
+area = (22/7) * (int(radius) ** 2)
+
+print('The area of the circle with radius:', radius, '=',area)
