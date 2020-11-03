979890f233429d4d718418f35ab296f8db87f257
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-11-03 12:18:45 +0100

    progress in lesson

--- area_calc.py
+++ area_calc.py
@@ -4,8 +4,27 @@
 
 #print('welcome ' + name)
 
-radius = input('What is the Radius of the circle?')
+# radius = input('What is the Radius of the circle?')
 
-area = (22/7) * (int(radius) ** 2)
+# area = (22/7) * (int(radius) ** 2)
 
-print('The area of the circle with radius:', radius, '=',area)
+# print('The area of the circle with radius:', radius, '=',area)
+
+# num1 = [90,78, 9, 13, 78, 9, 12]
+# num2 = [87, 7, 8, 9, 19, 0]
+# # del(num1[2])
+# # print(num1)
+
+# print ('first number is {0}, second number is {1}'.format(num1,num2))
+
+# print(f'first {num1}, second {num2} ')
+
+# age = input('Tell me your age:')
+
+# if int(age) < 18 :
+#     print(f'You have {18 - int(age)} years to be eligible to drive')
+
+# elif int (age) >= 18 :
+#     print('You are eligible to drive 👍🏼')
+# else :
+#     print('Kindly input a valid age')
